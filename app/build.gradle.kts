f19cd085a3133237f310efdf8949455a97762417
Jose Mejia Corredor <jose.mejiac@upb.edu.co>
2025-11-11 09:02:38 -0500

    chore(build): actualizar configuración de Gradle del módulo app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("com.caverock:androidsvg-aar:1.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
